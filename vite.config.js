805d1259c9c6e9359f624543c3c1c02d21d422a6
elinfelicia <elinratcliff@icloud.com>
2026-08-28 14:08:12 +0200

    add playwright testing - start

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // config from a starter template, seems to work /M
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    include: ['src/**/*.test.js']
+  }
 })
