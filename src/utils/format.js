4b2a083adc695f22a302a06901118ed20e4bb6b2
elinfelicia <elinratcliff@icloud.com>
2026-08-28 10:25:30 +0200

    format price and add testing implement formatted amounts on invoices view

---
+++ src/utils/format.js
@@ -0,0 +1,10 @@
+export const formatAmount = (amount) => {
+    const hasDecimals = amount % 1 !== 0;
+
+    if (amount === undefined || amount === null) return '-';
+
+    return amount.toLocaleString('sv-SE', {
+        minimumFractionDigits: hasDecimals ? 2 : 0,
+        maximumFractionDigits: 2,
+    })
+}
